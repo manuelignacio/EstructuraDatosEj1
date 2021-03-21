c8d4e6c6962aee40b4c067e55faf29d371037ef0
Manuel Lopez <manuelopez.30@hotmail.com>
2021-03-21 16:45:39 -0300

    Ejercicio estructura de datos en C

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+char nombre[50];
+int edad;
+char sexo[50];
+char club[50];
+}Corredor;
+
+
+int main()
+{
+
+    Corredor jugador1;
+    printf("Ingrese el nombre del corredor: ");
+    scanf("%s", jugador1.nombre);
+    printf("\n Ingrese la edad del corredor: ");
+    scanf("%d", &jugador1.edad);
+    printf("\n Ingrese el sexo del corredor: ");
+    scanf("%s", jugador1.sexo);
+    printf("\n Ingrese el club del corredor: ");
+    scanf("%s", jugador1.club);
+    if(jugador1.edad <= 18){
+        printf("Los datos del jugador son: \n");
+    printf(" %s \n", jugador1.nombre);
+    printf(" %d \n", jugador1.edad);
+    printf(" %s \n", jugador1.sexo);
+    printf(" %s \n", jugador1.club);
+    printf(" y su clase es Juvenil");
+
+    }
+    if(jugador1.edad > 18 && jugador1.edad <= 40){
+        printf("Los datos del jugador son: \n");
+    printf(" %s \n", jugador1.nombre);
+    printf(" %d \n", jugador1.edad);
+    printf(" %s \n", jugador1.sexo);
+    printf(" %s \n", jugador1.club);
+    printf(" y su clase es Senior");
+
+    }
+    if(jugador1.edad >40){
+            printf("Los datos del jugador son: \n");
+    printf(" %s \n", jugador1.nombre);
+    printf(" %d \n", jugador1.edad);
+    printf(" %s \n", jugador1.sexo);
+    printf(" %s \n", jugador1.club);
+    printf(" y su clase es Veterano");
+    }
+
+
+
+    return 0;
+}
